3740c8c3ac9c0d1e23ce5674a0180c8042e3e409
Gladys Annete Canche Gil <20470323@campeche.tecnm.mx>
2022-01-07 20:48:26 -0600

    Agrega el manejo de eventos que da comportamiento a la interfaz de usuario de la aplicacion

--- ImcApp/ImcApp/MainPage.xaml.cs
+++ ImcApp/ImcApp/MainPage.xaml.cs
@@ -5,14 +5,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using CalculadoraImc.App.Models;
 
 namespace ImcApp
 {
+
     public partial class MainPage : ContentPage
     {
+        private CalculadoraImc imc;
         public MainPage()
         {
             InitializeComponent();
+            LimpiarIU();
+        }
+
+        private void LimpiarIU()
+        {
+            PesoEntry.Text = string.Empty;
+            EstaturaEntry.Text = string.Empty;
+            ImcLabel.Text = string.Empty;
+            SituacionNutricionalLabel.Text = string.Empty;
+        }
+
+        private string GetEstadoNutricional(CalculadoraImc.EstadoNutricional estado)
+        {
+            switch (estado)
+            {
+                case CalculadoraImc.EstadoNutricional.PesoBajo:
+                    return "Peso bajo";
+                case CalculadoraImc.EstadoNutricional.PesoNormal:
+                    return "Peso normal";
+                case CalculadoraImc.EstadoNutricional.SobrePeso:
+                    return "Sobrepeso";
+                case CalculadoraImc.EstadoNutricional.Obesidad:
+                    return "Obesidad";
+                case CalculadoraImc.EstadoNutricional.ObesidadExtrema:
+                    return "Obesidad extrema";
+                default:
+                    return string.Empty;
+            }
+        }
+
+
+        private void CalcularButton_Clicked(object sender, EventArgs e)
+        {
+            decimal peso;
+            decimal estatura;
+            bool pesoEsConvertible = decimal.TryParse(pesoEntry.Text, out peso);
+            bool estaturaEsConvertible = decimal.TryParse(estaturaEntry.Text, out estatura);
+            if (pesoEsConvertible && estaturaEsConvertible)
+            {
+                CalculadoraImc cimc = new CalculadoraImc(peso, estatura);
+                imcLabel.Text = string.Format("{0:F4}", cimc.Imc);
+                situacionNutricionalLabel.Text =
+                GetEstadoNutricional(cimc.SituacionNutricional);
+            }
+            else
+            {
+                DisplayAlert(
+                "Alerta",
+                "El peso y la estatura deben ser valores numéricos.",
+                "Aceptar");
+            }
+
+            private void LimpiarButton_Clicked(object sender, EventArgs e)
+            {
+                LimpiarIU();
+            }
         }
     }
 }
